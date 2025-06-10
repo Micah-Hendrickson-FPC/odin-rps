daadc5f716b68013aa215cac68465a823c54ef76
Micah-Hendrickson-FPC <micah.hendrickson@fivepackcreative.com>
2025-06-10 14:10:49 -0500

    Add playGame and playRound functionality

--- rps.js
+++ rps.js
@@ -1,43 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
-let continuePlaying = true;
+"use strict";
 
-while(continuePlaying) {
-    //Get the human and computer choices
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-
-    //Determine the winner
-    let winner = getWinner(humanChoice, computerChoice);
-    
-    //Tell the user the game is being played
-    console.log("Rock\nPaper\nScissors\nShoot!\n");
-
-    //Show the choices
-    console.log(`You threw ${humanChoice}`);
-    console.log(`The computer threw ${computerChoice}`);
-
-    //Show the winner and update the scores
-    if (winner == "Tie") {
-        console.log("You tied... :/");
-    } else if (winner == "Player") {
-        console.log(`You won! ${humanChoice} beats ${computerChoice.toLowerCase()}.`);
-        humanScore++;
-    } else {
-        console.log(`You lost. ${computerChoice} beats ${humanChoice.toLowerCase()}.`);
-        computerScore++;
-    }
-    
-    //Display the scores
-    console.log(`Current scores:\nYou: ${humanScore}\nComputer: ${computerScore}`);
-
-    //Ask the user if they want to play again
-    let playAgain = prompt("Would you like to play again (y/n)? ");
-
-    if (playAgain.toLowerCase === "n") {
-        continuePlaying = false;
-    }
-}
+playGame();
 
 function getComputerChoice() {
     //Return a random value from [Rock, Paper, Scissors]
@@ -48,15 +11,17 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let validChoice = false;
+    let playerChoice;
     while(!validChoice) {
         //Prompt the user for their choice and store it in a variable
-        let playerChoice = prompt("What do you want to throw (Rock, Paper, or Scissors)? ").toLowerCase();
+        playerChoice = prompt("What do you want to throw (Rock, Paper, or Scissors)? ").toLowerCase();
         
-
         //Verify that the user entered a valid option
-        if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
+        if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
             //Capitalize the player's valid choice
             playerChoice = playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1);
+            //Update the sentinel value
+            validChoice = true;
         } else {
             console.log("That is not a valid option. Please select either rock, paper, or scissors.");
         }
@@ -96,4 +61,52 @@ function getWinner(humanChoice, computerChoice) {
 
     //Return the result
     return result;
+}
+
+function playRound(humanScore, computerScore) {
+    //Get the human and computer choices
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+
+    //Determine the winner
+    let winner = getWinner(humanChoice, computerChoice);
+    
+    //Tell the user the game is being played
+    console.log("Rock\nPaper\nScissors\nShoot!\n");
+
+    //Show the choices
+    console.log(`You threw ${humanChoice}`);
+    console.log(`The computer threw ${computerChoice}`);
+
+    //Show the winner and update the scores
+    if (winner == "Tie") {
+        console.log("You tied... :/");
+    } else if (winner == "Player") {
+        console.log(`You won! ${humanChoice} beats ${computerChoice.toLowerCase()}.`);
+        humanScore++;
+    } else {
+        console.log(`You lost. ${computerChoice} beats ${humanChoice.toLowerCase()}.`);
+        computerScore++;
+    }
+    
+    //Display the scores
+    console.log(`Current scores:\nYou: ${humanScore}\nComputer: ${computerScore}`);
+}
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playRound(humanScore, computerScore);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Congragulations! You won.");
+    } else if (humanScore < computerScore) {
+        console.log("You lost to the machine. Better luck next time.");
+    } else {
+        console.log("Its a draw!")
+    }
+
 }
